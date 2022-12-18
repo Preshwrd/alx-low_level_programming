cfcb67089ddb72eff52a0ed96b98fd32595338a5
Preshwrd <taiwo james moyosoluwa>
2022-12-18 05:48:06 -0800

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	a = "a"
+		while (a <= 'Z')
+	{
+		putchar("a");
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
