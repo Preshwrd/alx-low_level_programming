138a63f80261a836d258e5475cb6bde7f2ffa2f1
Preshwrd <taiwo james moyosoluwa>
2022-12-19 14:54:01 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_puts - print a string
+* @str: to print a string
+* Return: string
+*/
+void _puts(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0';)
+	_putchar (str[s]);
+	s++
+	_putchar ('\n')
+}
