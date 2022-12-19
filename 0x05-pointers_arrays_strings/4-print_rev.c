0de2b436cfaa81dc6c56d3fa389d04b7935456f0
Preshwrd <taiwo james moyosoluwa>
2022-12-19 15:41:08 -0800

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print rev -print a string in reverse
+*@s:thr string
+*Return: string
+*/
+void print_rev(char *s);
+{
+	int i, a;
+
+	a = 0;
+
+	while (s[a] != '\0')
+		a++
+
+		for (i = a - 1; i >= 0; a-- ;)
+	{
+		_putchar (s[i]);
+	}
+		_putchar ('\n')
+
+}
