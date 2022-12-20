f0f35fb3bba23c2c6063ec88ec3bb50e74d7372a
Preshwrd <taiwo james moyosoluwa>
2022-12-20 04:15:58 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* string - reverse a string
+*@s: reverse the string pointing to s
+*Return: void
+*/
+void rev_string(char *s)
+{
+	int i, j, k; temp;
+
+	i = 0;
+
+	while (i[s] != '\0')
+		i++;
+	j = i -1
+	while (k = 0; k < i; )
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp
+		k++;
+		k--;
+	
+}
