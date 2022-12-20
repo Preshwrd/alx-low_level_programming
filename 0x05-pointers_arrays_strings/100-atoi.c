6027f4b627deff241fda56ed5367114cfc51dd5c
Preshwrd <taiwo james moyosoluwa>
2022-12-20 13:46:12 -0800

    advance

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-* int _atoi - convert a string to integer
+* int_atoi - convert a string to integer
 *@s: pointing to the string to b converted
 */
 int _atoi(char *s)
@@ -16,14 +16,14 @@ int _atoi(char *s)
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if (*temp == '_')
+		if (*temp == '-')
 		sign *= -1;
 		temp++;
 	}
 	if (*temp != '\0')
 	{
 		do {
-				n = n *10 + (*temp - '0');
+				n = n * 10 + (*temp - '0');
 				temp++;
 			} while (*temp >= '0' && *temp <= '9');
 	}
