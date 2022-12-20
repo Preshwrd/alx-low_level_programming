638492d3a38b7692d8a954ebbcf062c9f45af988
Preshwrd <taiwo james moyosoluwa>
2022-12-20 07:29:28 -0800

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio>
+/**
+* puts_half -print half of a string
+*@str: print half of the string and if its odd print last n
+*/
+void puts_half(char *str);
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+		J = i / 2;
+	if (j % 2 == 1)
+		j++;
+
+		while (j < i)
+		{
+			_putchar (str[j]);
+			j++;
+		}
+
+	_putchar ('\n')
+
+
+}
