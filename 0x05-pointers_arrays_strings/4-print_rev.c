075ed53b95abf5d8210c7de4c44c98f7d1953d4c
Preshwrd <taiwo james moyosoluwa>
2022-12-20 03:23:17 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,11 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++);
-
-		for (a = a - 1; i != '\0'; a--)
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	for (a = a - 1; i != '\0'; a--)
 	{
-		_putchar (s[a]);
+	_putchar (s[a]);
 	}
-		_putchar ('\n');
-
+	_putchar ('\n');
 }
