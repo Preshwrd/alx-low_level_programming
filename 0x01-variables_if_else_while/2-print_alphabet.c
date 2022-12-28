e482005f71121347250ef67820fa1ac396917795
Preshwrd <taiwo james moyosoluwa>
2022-12-28 05:46:52 -0800

    alhabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*putchar - to print all the alphabet in lower case 
+*main - to print all the alphabet in lower case
 *Return: Always 0
 */
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; ++c)
 	{
 		putchar (c);
