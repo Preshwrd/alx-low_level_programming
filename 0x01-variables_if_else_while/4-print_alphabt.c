791f65f6bf527241cf18ce01cb6b9f661652f1d4
Preshwrd <taiwo james moyosoluwa>
2022-12-28 07:16:15 -0800

    excempt some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - print all alphabet except q and e
+*Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	if ((c != 'q' && c != 'e') && c <= 'z')
+	putchar (c);
+	c++;
+	putchar ('\n');
+	return (0);
+}
