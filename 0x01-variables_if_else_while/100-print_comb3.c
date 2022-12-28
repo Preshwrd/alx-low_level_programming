42cd62f5fff7137ca8f351d9973bf993cc93eb9f
Preshwrd <taiwo james moyosoluwa>
2022-12-28 14:42:32 -0800

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main- prints all possible different combinations of two digits;
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int t;
+	int o;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (o = t + 1; o <= 9; o++)
+		{
+			putchar (t + '0');
+			putchar (o + '0');
+
+			if (t < 8)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
