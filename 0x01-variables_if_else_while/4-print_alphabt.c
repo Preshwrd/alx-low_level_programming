7064be324a5e3b5f6753633789a926084fa38803
Preshwrd <taiwo james moyosoluwa>
2022-12-28 07:24:29 -0800

    excempt some letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,10 @@ int main(void)
 
 	c = 'a';
 	while (c <= 'z')
-	if ((c != 'q' && c != 'e') && c <= 'z')
-	putchar (c);
-	c++;
-	putchar ('\n');
+	{	if ((c != 'q' && c != 'e') && c <= 'z')
+		putchar (c);
+		c++;
+	}
+		putchar ('\n');
 	return (0);
 }
