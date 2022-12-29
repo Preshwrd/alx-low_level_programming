f79f171436cb074dbf5933197b6e1d29c474a874
Preshwrd <taiwo james moyosoluwa>
2022-12-29 07:59:46 -0800

    alhabetuper and lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_isalpha- check for alphabetic character
+*
+*@c: to check if its lowar case or not
+*
+*Return: 1 if its lower or upper case else 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
