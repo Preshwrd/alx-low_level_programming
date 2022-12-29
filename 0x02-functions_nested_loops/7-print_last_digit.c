1c9b040ee2efd72ccc870313746a4117ed582698
Preshwrd <taiwo james moyosoluwa>
2022-12-29 11:25:47 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_last_digit- to print thr last digit of a number
+*
+*@d: the last digit
+*
+*Return: last digit
+*/
+int print_last_digit(int d)
+{
+		return (d % 10);
+}
