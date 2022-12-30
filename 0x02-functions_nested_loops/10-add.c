95e40c352302694bb71be845a4c1bf0f7ebc2c66
Preshwrd <taiwo james moyosoluwa>
2022-12-30 06:32:35 -0800

    alhabetuper and lower

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
 *add- add two number and return
+*@i: numbers to add
+*@n: numbers to sum
 * return: the sum of the number
 */
 int add(int i, int n)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,25 @@
 */
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int n, m, p;
 
-	for (x = 0; x <= 9; x++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (y = 0; y <= 9; y++)
+		_putchar ('0');
+
+		for (m = 1; m <= 9; m++)
 		{
-			z = x = y;
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-				_putchar (44);
-				 _putchar (32);
-				 _putchar (d + '0');
-				 _putchar (u + '0');
-				}
-					else
-					{
-					if (y != 0)
-					{
-						_putchar (44);
-						_putchar (32);
-						_putchar (32);
-					}
-						 _putchar (z + '0');
-					}
+			_putchar (',');
+			_putchar (' ');
+
+			p = n * m;
+
+			if (p <= 9)
+				_putchar (' ');
+			else
+			{	_putchar ((n / 10) + '0');
+				 _putchar ((n % 10) + '0');
+			}
 		}
 			 _putchar('\n');
 	}
