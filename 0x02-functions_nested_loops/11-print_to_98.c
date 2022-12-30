a55451ba705657cbe79b2acbefed46daa65323ba
Preshwrd <taiwo james moyosoluwa>
2022-12-30 07:50:30 -0800

    alhabetuper and lower

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,6 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-		
 			printf("%d, ", n--);
 			printf("%d", n);
 		
@@ -21,10 +20,8 @@ void print_to_98(int n)
 	else
 	{
 		while (n < 98)
-		{
 			printf("%d, ", n++);
 			printf("%d", n);
-		}
 	}
 	_putchar ('\n');
 }
