a9ce72f87c66001c92182b2f30eb0ad73f25ab29
Preshwrd <taiwo james moyosoluwa>
2022-12-30 06:51:03 -0800

    alhabetuper and lower

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,10 +21,8 @@ void times_table(void)
 			if (p <= 9)
 				_putchar (' ');
 			else
-			{
-				_putchar ((p / 10) + '0');
-				_putchar ((p % 10) + '0');		
-			}
+			_putchar ((p / 10) + '0');
+			_putchar ((p % 10) + '0');
 		}
 			 _putchar('\n');
 	}
