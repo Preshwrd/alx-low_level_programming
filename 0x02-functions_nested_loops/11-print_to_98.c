5e1280f0c47de528cf5c7bd1427fa57a3d506c2b
Preshwrd <taiwo james moyosoluwa>
2022-12-30 07:24:47 -0800

    alhabetuper and lower

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print _to_98- print all natural numbers from 
+*n to 98
+*seperated by comma and space 
+*last number should b 98
+*@n: number to b printed out 
+*Return: all natural numbers 
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		printf ("%d, ", n--);
+		printf ("%d/n", n);
+	}
+	if else (n < 98) 
+	{
+		printf ("%d, ", n++);
+		printf ("%d/n", n);
+	}
+		
+	_putchar ('\n');
+}
+
