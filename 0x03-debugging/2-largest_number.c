1bc4c018cdb0550535f05b409f3e42b2bbe3f2c9
Preshwrd <taiwo james moyosoluwa>
2023-01-01 07:53:52 -0800

    alhabetuper and lower

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,18 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		if	(a > c)
 		{
-		largest = a;
+			largest = a;
 		}
 		else
 		{
-		largest = c;
+			largest = c;
 		}
 	}
     else if (b > c)
 		{
-
         	largest = b;
-   		 }	
+   		}	
 	else
 	{
 		largest = c;
