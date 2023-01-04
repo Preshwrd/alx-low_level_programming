698b007ce608fab8c500d53371c6a9d099969723
Preshwrd <taiwo james moyosoluwa>
2023-01-04 06:29:29 -0800

    add

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *_print_rev_recursion - print a string in reverse
 *@s:pointer of the string to be printed 
@@ -6,9 +7,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-    if(*s)
-    {
-        putchar (*s);
-        _print_rev_recursion (s + 1);
-    }
+	if(*s)
+	{
+		putchar (*s);
+		_print_rev_recursion (s + 1);
+	}
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,7 +8,7 @@
 */
 int _pow_recursion(int x, int y)
 {
-    int val = x;
+    int result =x;
 
   if (y < 0)
       return (-1);
