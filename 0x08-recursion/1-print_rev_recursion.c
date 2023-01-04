cd8647431c019df93ea65e04e389e777eafdebdb
Preshwrd <taiwo james moyosoluwa>
2023-01-04 07:00:13 -0800

    added

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 /**
 *_print_rev_recursion - print a string in reverse
-*@s:pointer of the string to be printed 
+*@s:pointer of the string to be printed
 *
 */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
-		_print_rev_recursion (s + 1);
+		_print_rev_recursion(s + 1);
 		putchar (*s);
 	}
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,9 @@
 */
 int factorial(int n)
 {
-  int result = n;
 
-  if (n < 0)
-    
-     return (-1);
-    
-  else if (n >= 0 && n <= 1)
-    
-      return (1);
-    
-}
+	if (n < 0)
+	return (-1);
+
+	else if (n >= 0 && n <= 1)
+	return (1);
